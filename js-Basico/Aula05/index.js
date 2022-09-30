5dd149acd5c6a2534841fb98c62d7c487a915175
Hyarlei <hyarleysf@gmail.com>
2022-09-30 19:45:17 -0300

    Aulas js - basico

--- js-Basico/Aula05/index.js
+++ js-Basico/Aula05/index.js
@@ -1,4 +1,9 @@
-//um comentário
+//Não podemos criar variáveis com palavras reservadas.
+//Variáveis precisam ter nomes significativos.
+//Não pode começar o nome de uma variável com um número.
+//Não pode conter espaços ou traços.
+//Ultilizamos camelCase.
+//case sensitive.
 console.log("Olá mundo!");
 console.log("Esta mensagem é vista pelo console do navegador.");
-//alert("olá mundo!");
+alert("olá mundo!");
