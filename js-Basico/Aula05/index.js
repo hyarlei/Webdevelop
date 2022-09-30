51701a83a6296d6c221f0d754a0da83783f0f72f
Hyarlei <hyarleysf@gmail.com>
2022-09-30 19:01:10 -0300

    Aulas js-básico

---
+++ js-Basico/Aula05/index.js
@@ -0,0 +1,4 @@
+//um comentário
+console.log("Olá mundo!");
+console.log("Esta mensagem é vista pelo console do navegador.");
+alert("olá mundo!");
---
+++ js-Basico/Aula06/index.js
@@ -0,0 +1,7 @@
+let nome = 'Hyarlei'; //String
+
+console.log(nome, 'nasceu em 2004.');
+console.log('Em 2050 ' + nome,'conheceu Maria.');
+console.log(nome,'casou-se com Maria em 2040.');
+console.log('Maria teve 1 filho com ' + nome,'em 2060.');
+console.log('O filho de ' + nome,'se chama Geovana.');
