657b10da675a6a171288a27403fc6944ff7ca0d9
Hyarlei <hyarleysf@gmail.com>
2022-10-06 12:51:12 -0300

    Aulas de js - basico

---
+++ js-Basico/Aula09/index.js
@@ -0,0 +1,9 @@
+//string, number, undefined, null, boolean, Symbol - Dados primitivos
+const nome = 'Luiz'; //string
+const nome1 = "Luiz"; //string
+const nome2 = `Luiz`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória.
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória.
+const boolean = true; // true ou false (lógico)
---
+++ js-Basico/Aula10/index.js
@@ -0,0 +1,39 @@
+/* Operadores e incrememtos Artimétcos, de operadores e incrementos
+* Artimétcos 
+* Adição / concatenação
+* Potenciação
+* Resto da divisçao
+* Incremento = ++
+* Decremento = --
+*/
+
+const num1 = 10;
+const num2 = 2;
+console.log("O resultado da potenciação é: "+ (num1 ** num2)); //potenciação
+
+const num3 = 10;
+const num4 = 3;
+console.log("O resto da divisão é: "+ (num3 % num4)); //divisão
+
+const num5 = 10;
+const num6 = 5;
+console.log("O resultado da soma é: " + (num5 + num6)); //soma 
+
+const num7 = 10;
+const num8 = 10;
+console.log("O resultado subtração é: "+ (num7 - num8)); //subtração
+
+const num9 = 10;
+const num10 = 11;
+console.log("O resultado da multiplicaçaõ é: "+ (num9 * num10)); //multiplicaçaõ
+
+let incremento = 1;
+incremento++;
+console.log("O resultado do incremento é: "+ incremento);
+
+const num11 = 10;
+const num12 = parseInt('5'); // ou const num12 = parseFloat('5.2'); 
+console.log("O resultado da soma: "+ (num11 + num12));
+console.log(typeof num2);
+//parseInt(inteiros);  parseFloat(decimais);
+
