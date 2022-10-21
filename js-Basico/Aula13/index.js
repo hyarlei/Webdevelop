4acf65789bebfff6fd5419962b9afd03915312cd
Hyarlei <hyarleysf@gmail.com>
2022-10-21 16:49:35 -0300

    Aula 13 de JS Básico

---
+++ js-Basico/Aula13/index.js
@@ -0,0 +1,7 @@
+/*               0123456789 0123456789 0123456789 01234
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+console.log(umaString.replace(/r/g, "#"));
+*/
+let umaString = "O rato roeu a roupa do rei de roma.";
+console.log(umaString.length - 3);
