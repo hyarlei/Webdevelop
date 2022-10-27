5b181d1e171726c6e00fd1da399f3facd102a7c1
Hyarlei <hyarleysf@gmail.com>
2022-10-27 19:16:26 -0300

    Aula arrayList e funções

---
+++ js-Basico/Aula17/index.js
@@ -0,0 +1,39 @@
+const alunos = ['hyarlei', 'caio', 'thiago'];
+
+alunos.push('Luiza');
+alunos.push('Eduardo');
+console.log(alunos.slice(0, -1));
+
+//removendo do começo
+//const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+//removendo pelo fim
+// const removido = alunos.pop();
+// console.log(removido);
+// console.log(alunos);
+
+//Adicionando novos elementos ao array
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+//Adiciona no fim
+// alunos.push('Luiza');
+// alunos.push('Fábio');
+
+//Adiciona no começo
+// alunos.unshift('Maria');
+// alunos.unshift('José');
+
+//Printando os elementos do array
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
+
+//modoficando os elementos do array
+// alunos[0] = 'Francisco';
+
+//printando os elementos do array completamente
+// console.log(alunos);
---
+++ js-Basico/Aula18/index.js
@@ -0,0 +1,38 @@
+//Funções
+
+// function saudacao(nome){
+//     console.log(`Bom dia ${nome}!`);
+// }
+
+// saudacao("Luiz");
+
+// function saudacao(nome){
+//     return `Bom dia ${nome}!`;// Função retorna a frase com a variável criada passada como parametro
+// } 
+
+// const variavel = saudacao("Luiz");
+// console.log(variavel);
+
+// function soma(x, y){//Função
+//     const resultado = x + y; //Fazer a soma entre dois numeros
+//     return resultado;
+// }
+
+// const resultado = soma('Hyarlei', 'Silva');
+// console.log(resultado);
+
+// console.log(soma(2, 2));//printando e repassado os valores dentro dos parametros
+// console.log(soma(10, 5));
+// console.log(soma(4, 6));
+
+// const raiz = function(n){
+//     return n ** 0.5;
+// };
+
+const raiz = function(n){
+    return Math.sqrt(n);
+}
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
