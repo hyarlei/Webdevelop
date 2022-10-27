dddaafc601f3424f5ca3585bb6afb392c3018f5b
Hyarlei <hyarleysf@gmail.com>
2022-10-27 12:42:50 -0300

    Aula 16 -  exercicios prático

---
+++ js-Basico/Aula15/index.js
@@ -0,0 +1,16 @@
+//let num1 = 9.54578;
+//let num2 = Math.floor(num1); //Fazer a aproximação pelo número menor
+//console.log(num2);
+
+//let num1 = 9.54578;
+//let num2 = Math.ceil(num1); //Fazer a aproximação pelo número maior
+//console.log(num2);
+
+//let num2 = Math.round(num1); //Fazer a aproximação automaticamente.
+// console.log(num2);
+
+//console.log(Math.max(1, 2, 3, 4, 5, -40, 30, 1500, -435, 6, 7)); // 1500
+// console.log(Math.min(1, 2, 3, 4, 5, -40, 30, 1500, -435, 6, 7)); // -435
+
+let num1 = 9;
+console.log(num1 ** (1/2)); // Elevar algum número
