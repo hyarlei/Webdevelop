5e0140870661d4a44fcc7b38200496ca3a078711
Hyarlei <hyarleysf@gmail.com>
2022-10-27 20:05:25 -0300

    Aula -  Objetos

---
+++ js-Basico/Aula19/index.js
@@ -0,0 +1,23 @@
+// const nome01 = 'Hyarlei';
+// const sobrenome01 = 'Silva';
+// const idade01 = '18';
+
+// const nome02 = 'Maria';
+// const sobrenome02 = 'Oliveira';
+// const idade02 = '18';
+
+//outra maneira de fazer essa mesma coisa é usando objetos.
+const pessoa1 ={//Objeto
+    nome: 'Hyarlei', 
+    sobrenome: 'Silva',
+    idade: 18
+};
+
+const pessoa2 ={//Objeto
+    nome: 'Maria',
+    sobrenome: 'seideque',
+    idade: 18
+};
+
+console.log(pessoa1);
+console.log(pessoa2);
