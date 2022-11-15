271f5d9c906ed2e1dad7184595d773081175f2d7
Hyarlei <hyarleysf@gmail.com>
2022-11-15 09:46:13 -0300

    For - Estrutura de repetição

---
+++ js-Basico/Aula33/index.js
@@ -0,0 +1,9 @@
+// for(let i = 0; i <= 10; i++){
+//     const par = i % 2 === 0 ? "PAR" : "IMPAR";
+//     console.log(i, par);
+// }
+const Frutas = ["Maça", "Pêra", "Uva", "Banana"];
+
+for(let i = 0; i < Frutas.length; i++) {
+    console.log(`Indice ${i}:`, Frutas[i]);
+}
