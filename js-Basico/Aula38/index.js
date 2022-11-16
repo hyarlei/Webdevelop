691055e80adb1d0dde03a701122f17d13f37f7e5
Hyarlei <hyarleysf@gmail.com>
2022-11-16 16:26:08 -0300

    While e Do While - Estrutura de repetição

---
+++ js-Basico/Aula38/index.js
@@ -0,0 +1,24 @@
+// let i = 0;
+
+// while (i <= 10){
+//     console.log(i);
+//     i++;
+// }
+function random(min, max){
+    const r = Math.random() * (min, max) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand = 10;
+
+while(rand !== 10){
+    console.log(rand);
+}
+
+console.log("#####");
+
+do{
+    console.log(rand);
+}while(rand !== 10);
