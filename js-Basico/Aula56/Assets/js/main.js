71e3b62c01d602079895d4666e48bd9d973438cc
Hyarlei <hyarleysf@gmail.com>
2022-12-21 19:48:37 -0300

    Praticando com factory functions

--- js-Basico/Aula56/Assets/js/main.js
+++ js-Basico/Aula56/Assets/js/main.js
@@ -1,86 +1,88 @@
 function criaCalculadora() {
     return {
-        display: document.querySelector(".display"),
-
-        inicia() {
-            this.cliqueBotoes();
-            this.pressionaBackSpace();
-            this.PressionaEnter();
-        },
-
-        pressionaBackSpace() {
-            this.display.addEventListener('keydown', e => {
-              if (e.keyCode === 8) {
-                e.preventDefault();
-                this.clearDisplay();
-              }
-            });
-          },
-
-        PressionaEnter(){
-            this.display.addEventListener("Keyup", e => {
-                if(e.keyCode === 13){
-                    this.RealizaConta();
-                }
-            });
-        },
-
-        RealizaConta() {
-            let conta = this.display.value;
-
-            try{
-                conta = eval(conta);
-
-                if(!conta){
-                    alert("Conta Inválida!");
-                    return;
-                }
-
-                this.display.value = String(conta);
-            } catch(e){
-                alert("Conta Inválida!");
-                return;
-            }
-        },
-
-        clearDisplay() {
-            this.display.value = "";
-        },
-
-        ApagaUm() {
-            this.display.value = this.display.value.slice(0, -1);
-        },
-
-        cliqueBotoes() {
-            document.addEventListener("Click", e => {
-                const el = e.target;
-                console.log(this);
-
-                if (el.classList.contains("btn-num")) {
-                    this.btnParaDisplay(el.innerText);
-                }
-
-                if (el.classList.contains("btn-clear")) {
-                    this.clearDisplay();
-                }
-
-                if (el.classList.contains("btn-del")) {
-                    this.ApagaUm();
-                }
-
-                if (el.classList.contains("btn-eq")) {
-                    this.RealizaConta();
-                }
-
-                // this.display.focus();
-            });
-        },
-
-        btnParaDisplay(valor) {
-            this.display.value += valor;
+      display: document.querySelector('.display'),
+  
+      inicia() {
+        this.cliqueBotoes();
+        this.pressionaBackSpace();
+        this.pressionaEnter();
+      },
+  
+      pressionaBackSpace() {
+        this.display.addEventListener('keydown', e => {
+          if (e.keyCode === 8) {
+            e.preventDefault();
+            this.clearDisplay();
+          }
+        });
+      },
+  
+      pressionaEnter() {
+        this.display.addEventListener('keyup', e => {
+          if (e.keyCode === 13) {
+            this.realizaConta();
+          }
+        });
+      },
+  
+      realizaConta() {
+        let conta = this.display.value;
+  
+        try {
+          conta = eval(conta);
+  
+          if (!conta) {
+            alert('Conta inválida');
+            return;
+          }
+  
+          this.display.value = String(conta);
+        } catch (e) {
+          alert('Conta inválida');
+          return;
         }
+      },
+  
+      clearDisplay() {
+        this.display.value = '';
+      },
+  
+      apagaUm() {
+        this.display.value = this.display.value.slice(0, -1);
+      },
+  
+  
+      cliqueBotoes() {
+        document.addEventListener('click', e => {
+          const el = e.target;
+  
+          if (el.classList.contains('btn-num')) {
+            this.btnParaDisplay(el.innerText);
+          }
+  
+          if (el.classList.contains('btn-Clear')) {
+            this.clearDisplay();
+          }
+  
+          if (el.classList.contains('btn-del')) {
+            this.apagaUm();
+          }
+  
+          if (el.classList.contains('btn-eq')) {
+            this.realizaConta();
+          }
+  
+          this.display.focus();
+        });
+      },
+  
+      btnParaDisplay(valor) {
+        this.display.value += valor;
+      }
+  
     };
-}
-
-const Calculadora = criaCalculadora();
-Calculadora.inicia();
+  }
+  
+  const calculadora = criaCalculadora();
+  calculadora.inicia();
+  
